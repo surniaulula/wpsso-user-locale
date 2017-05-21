3ce752cfe43794e6c101e26be59bdf24a60a35e8
JS Morisset <jsm@surniaulula.com>
2017-05-21 17:51:12 +0200

    v1.1.6

--- lib/config.php
+++ lib/config.php
@@ -27,7 +27,7 @@ if ( ! class_exists( 'WpssoUlConfig' ) ) {
 					'domain_path' => '/languages',
 					'req' => array(
 						'short' => 'WPSSO',
-						'name' => 'WPSSO (Social Sharing Optimization)',
+						'name' => 'WPSSO',
 						'min_version' => '3.42.0',
 					),
 					'img' => array(
