b0535d910eb45aad82a07a9945258bf57c3027f6
JS Morisset <jsm@surniaulula.com>
2017-09-30 11:10:42 -0400

    v1.1.7

--- lib/config-gettext-extract.php
+++ lib/config-gettext-extract.php
@@ -1,5 +1,5 @@
 <?php die( 'These aren\'t the droids you\'re looking for...' );
-// extract of translatable text strings from static config array(s)
+// extract of translatable text strings from static config array
 _x( 'User Locale', 'lib file description', 'wpsso-user-locale' );
 _x( 'WPSSO extension to add a user locale / language / region selector in the WordPress admin back-end and front-end toolbar menus.', 'plugin description', 'wpsso-user-locale' );
 ?>
