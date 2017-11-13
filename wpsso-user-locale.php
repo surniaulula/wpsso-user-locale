881482222c3f59b2e5939157626965135cec3992
JS Morisset <jsm@surniaulula.com>
2017-11-13 08:41:20 -0600

    v1.1.9-rc.2

--- wpsso-user-locale.php
+++ wpsso-user-locale.php
@@ -14,7 +14,7 @@
  * Requires PHP: 5.3
  * Requires At Least: 3.7
  * Tested Up To: 4.9
- * Version: 1.1.9-rc.1
+ * Version: 1.1.9-rc.2
  *
  * Version Numbering: {major}.{minor}.{bugfix}[-{stage}.{level}]
  *
