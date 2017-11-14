282192a5414b068cc54f875fc68355cfb876ec3c
JS Morisset <jsm@surniaulula.com>
2017-11-14 11:02:30 -0600

    v1.1.9

--- wpsso-user-locale.php
+++ wpsso-user-locale.php
@@ -11,8 +11,8 @@
  * License: GPLv3
  * License URI: http://www.gnu.org/licenses/gpl.txt
  * Description: WPSSO Core extension to add a user locale / language / region selector in the WordPress admin back-end and front-end toolbar menus.
- * Requires PHP: 5.3
- * Requires At Least: 3.7
+ * Requires PHP: 5.4
+ * Requires At Least: 3.8
  * Tested Up To: 4.9
  * Version: 1.1.9
  *
