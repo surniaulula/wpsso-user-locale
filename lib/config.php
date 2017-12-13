2dc3d1f34c3babe2a3a6340eda9964151375952b
JS Morisset <jsm@surniaulula.com>
2017-12-13 08:40:46 -0600

    v1.1.9

--- lib/config.php
+++ lib/config.php
@@ -54,11 +54,7 @@ if ( ! class_exists( 'WpssoUlConfig' ) ) {
 		public static function get_version( $add_slug = false ) {
 			$ext = 'wpssoul';
 			$info =& self::$cf['plugin'][$ext];
-			if ( $add_slug ) {
-				return $info['slug'].'-'.$info['version'];
-			} else {
-				return $info['version'];
-			}
+			return $add_slug ? $info['slug'].'-'.$info['version'] : $info['version'];
 		}
 
 		public static function set_constants( $plugin_filepath ) { 
