6893c93bbb3e302fed293f8e2375fb704ddf0c80
JS Morisset <jsm@surniaulula.com>
2018-02-19 13:52:35 -0600

    v1.2.0-dev.2

--- lib/config.php
+++ lib/config.php
@@ -16,7 +16,7 @@ if ( ! class_exists( 'WpssoUlConfig' ) ) {
 		public static $cf = array(
 			'plugin' => array(
 				'wpssoul' => array(			// Plugin acronym.
-					'version' => '1.1.10',		// Plugin version.
+					'version' => '1.2.0-dev.2',		// Plugin version.
 					'opt_version' => '6',		// Increment when changing default option values.
 					'short' => 'WPSSO UL',		// Short plugin name.
 					'name' => 'WPSSO User Locale Selector',
@@ -29,7 +29,7 @@ if ( ! class_exists( 'WpssoUlConfig' ) ) {
 					'req' => array(
 						'short' => 'WPSSO Core',
 						'name' => 'WPSSO Core',
-						'min_version' => '3.52.0',
+						'min_version' => '3.53.1-dev.2',
 					),
 					'img' => array(
 						'icons' => array(
--- wpsso-user-locale.php
+++ wpsso-user-locale.php
@@ -14,7 +14,7 @@
  * Requires PHP: 5.4
  * Requires At Least: 3.8
  * Tested Up To: 4.9.4
- * Version: 1.1.10
+ * Version: 1.2.0-dev.2
  *
  * Version Numbering: {major}.{minor}.{bugfix}[-{stage}.{level}]
  *
@@ -146,6 +146,7 @@ if ( ! class_exists( 'WpssoUl' ) ) {
 		}
 
 		public function wpsso_get_config( $cf, $plugin_version = 0 ) {
+
 			$info = WpssoUlConfig::$cf['plugin']['wpssoul'];
 
 			if ( version_compare( $plugin_version, $info['req']['min_version'], '<' ) ) {
@@ -157,57 +158,62 @@ if ( ! class_exists( 'WpssoUl' ) ) {
 		}
 
 		public function wpsso_init_options() {
-			if ( method_exists( 'Wpsso', 'get_instance' ) ) {
-				$this->p =& Wpsso::get_instance();
-			} else {
-				$this->p =& $GLOBALS['wpsso'];
-			}
 
-			if ( $this->p->debug->enabled ) {
-				$this->p->debug->mark();
-			}
+			$this->p =& Wpsso::get_instance();
 
-			if ( $this->have_req_min ) {
-				$this->p->avail['p_ext']['ul'] = true;
-			} else {
-				$this->p->avail['p_ext']['ul'] = false;	// just in case
-			}
-		}
-
-		public function wpsso_init_objects() {
-			if ( $this->p->debug->enabled ) {
-				$this->p->debug->mark();
-			}
-
-			if ( $this->have_req_min ) {
-				$this->filters = new WpssoUlFilters( $this->p );
-				$this->locale = new WpssoUlLocale( $this->p );
-			}
-		}
-
-		public function wpsso_init_plugin() {
 			if ( $this->p->debug->enabled ) {
 				$this->p->debug->mark();
 			}
 
 			if ( ! $this->have_req_min ) {
-				return $this->min_version_notice();	// stop here
+				$this->p->avail['p_ext']['ul'] = false;	// just in case
+				return;	// stop here
+			}
+
+			$this->p->avail['p_ext']['ul'] = true;
+		}
+
+		public function wpsso_init_objects() {
+
+			if ( $this->p->debug->enabled ) {
+				$this->p->debug->mark();
+			}
+
+			if ( ! $this->have_req_min ) {
+				return;	// stop here
+			}
+
+			$this->filters = new WpssoUlFilters( $this->p );
+			$this->locale = new WpssoUlLocale( $this->p );
+		}
+
+		public function wpsso_init_plugin() {
+
+			if ( $this->p->debug->enabled ) {
+				$this->p->debug->mark();
+			}
+
+			if ( ! $this->have_req_min ) {
+				$this->min_version_notice();
+				return;	// stop here
 			}
 		}
 
 		private function min_version_notice() {
-			$info = WpssoUlConfig::$cf['plugin']['wpssoul'];
-			$wpsso_version = $this->p->cf['plugin']['wpsso']['version'];
 
-			if ( $this->p->debug->enabled ) {
-				$this->p->debug->log( $info['name'] . ' requires ' . $info['req']['short'] . ' v' . 
-					$info['req']['min_version'] . ' or newer (' . $wpsso_version . ' installed)' );
-			}
+			$info = WpssoUlConfig::$cf['plugin']['wpssoul'];
+			$have_version = $this->p->cf['plugin']['wpsso']['version'];
+
+			$error_msg = sprintf( __( 'The %1$s version %2$s extension requires %3$s version %4$s or newer (version %5$s is currently installed).',
+				'wpsso-user-locale' ), $info['name'], $info['version'], $info['req']['short'], $info['req']['min_version'], $have_version );
+
+			trigger_error( sprintf( __( '%s warning:', 'wpsso-user-locale' ), $info['short'] ).' '.$error_msg, E_USER_WARNING );
 
 			if ( is_admin() ) {
-				$this->p->notice->err( sprintf( __( 'The %1$s extension v%2$s requires %3$s v%4$s or newer (v%5$s currently installed).',
-					'wpsso-user-locale' ), $info['name'], $info['version'], $info['req']['short'],
-						$info['req']['min_version'], $wpsso_version ) );
+				$this->p->notice->err( $error_msg );
+				if ( method_exists( $this->p->admin, 'get_check_for_updates_link' ) ) {
+					$this->p->notice->inf( $this->p->admin->get_check_for_updates_link() );
+				}
 			}
 		}
 	}
