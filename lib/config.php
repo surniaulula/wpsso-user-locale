e541298d6c90ccd3ce21e095bee49f09ca093d79
JS Morisset <jsm@surniaulula.com>
2018-02-19 08:10:48 -0600

    v1.1.10

--- lib/config.php
+++ lib/config.php
@@ -27,7 +27,7 @@ if ( ! class_exists( 'WpssoUlConfig' ) ) {
 					'text_domain' => 'wpsso-user-locale',
 					'domain_path' => '/languages',
 					'req' => array(
-						'short' => 'WPSSO',
+						'short' => 'WPSSO Core',
 						'name' => 'WPSSO Core',
 						'min_version' => '3.52.0',
 					),
