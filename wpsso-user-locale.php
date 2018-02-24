c8edeb7cad0c64a80b5d8ce3656342c531fef311
JS Morisset <jsm@surniaulula.com>
2018-02-24 09:33:10 -0600

    v1.2.0-rc.3

--- wpsso-user-locale.php
+++ wpsso-user-locale.php
@@ -207,14 +207,14 @@ if ( ! class_exists( 'WpssoUl' ) ) {
 			$error_msg = sprintf( __( 'The %1$s version %2$s extension requires %3$s version %4$s or newer (version %5$s is currently installed).',
 				'wpsso-user-locale' ), $info['name'], $info['version'], $info['req']['short'], $info['req']['min_version'], $have_version );
 
-			trigger_error( sprintf( __( '%s warning:', 'wpsso-user-locale' ), $info['short'] ).' '.$error_msg, E_USER_WARNING );
-
 			if ( is_admin() ) {
 				$this->p->notice->err( $error_msg );
 				if ( method_exists( $this->p->admin, 'get_check_for_updates_link' ) ) {
 					$this->p->notice->inf( $this->p->admin->get_check_for_updates_link() );
 				}
 			}
+
+			trigger_error( sprintf( __( '%s warning:', 'wpsso-user-locale' ), $info['short'] ).' '.$error_msg, E_USER_WARNING );
 		}
 	}
 
