f75b08e6df95f5922b3065c98a32e44022ffa9d3
JS Morisset <jsm@surniaulula.com>
2018-02-24 11:49:03 -0600

    v1.2.0-rc.3

--- wpsso-user-locale.php
+++ wpsso-user-locale.php
@@ -208,7 +208,9 @@ if ( ! class_exists( 'WpssoUl' ) ) {
 				'wpsso-user-locale' ), $info['name'], $info['version'], $info['req']['short'], $info['req']['min_version'], $have_version );
 
 			if ( is_admin() ) {
+
 				$this->p->notice->err( $error_msg );
+
 				if ( method_exists( $this->p->admin, 'get_check_for_updates_link' ) ) {
 					$this->p->notice->inf( $this->p->admin->get_check_for_updates_link() );
 				}
