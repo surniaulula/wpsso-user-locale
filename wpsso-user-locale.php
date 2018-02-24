fcc1b74f7bc2d5d69b61d101ef331875abf312aa
JS Morisset <jsm@surniaulula.com>
2018-02-24 13:59:54 -0600

    v1.2.0

--- wpsso-user-locale.php
+++ wpsso-user-locale.php
@@ -216,7 +216,8 @@ if ( ! class_exists( 'WpssoUl' ) ) {
 				}
 			}
 
-			trigger_error( sprintf( __( '%s warning:', 'wpsso-user-locale' ), $info['short'] ).' '.$error_msg, E_USER_WARNING );
+			// translators: %s is the short plugin name
+			trigger_error( sprintf( __( '%s warning:', 'wpsso-user-locale' ), $info['short'] ).' '.rtrim( $error_msg, '.' ), E_USER_WARNING );
 		}
 	}
 
