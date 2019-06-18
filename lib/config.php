61a9d91f13750a56c9b2a92cb3567d516e6a821f
JS Morisset <jsm@surniaulula.com>
2019-06-18 13:16:57 -0700

    v2.0.0-dev.2

--- lib/config.php
+++ lib/config.php
@@ -16,7 +16,7 @@ if ( ! class_exists( 'WpssoUlConfig' ) ) {
 		public static $cf = array(
 			'plugin' => array(
 				'wpssoul' => array(			// Plugin acronym.
-					'version'     => '2.0.0-dev.1',	// Plugin version.
+					'version'     => '2.0.0-dev.2',	// Plugin version.
 					'opt_version' => '7',		// Increment when changing default option values.
 					'short'       => 'WPSSO UL',	// Short plugin name.
 					'name'        => 'WPSSO User Locale Selector',
@@ -29,7 +29,7 @@ if ( ! class_exists( 'WpssoUlConfig' ) ) {
 					'req'         => array(
 						'short'       => 'WPSSO Core',
 						'name'        => 'WPSSO Core',
-						'min_version' => '5.0.0-dev.1',
+						'min_version' => '5.0.0-dev.2',
 					),
 					'assets' => array(
 						'icons' => array(
