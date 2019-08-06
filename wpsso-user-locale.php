2b475235caaf581dabf3e435096675569cdeff71
JS Morisset <jsm@surniaulula.com>
2019-08-06 14:52:19 -0700

    v2.0.1-dev.3

--- wpsso-user-locale.php
+++ wpsso-user-locale.php
@@ -13,7 +13,7 @@
  * Description: Quick and easy locale / language / region selector for the WordPress admin toolbar.
  * Requires At Least: 4.7
  * Tested Up To: 5.2.2
- * Version: 2.0.1-dev.2
+ * Version: 2.0.1-dev.3
  *
  * Version Numbering: {major}.{minor}.{bugfix}[-{stage}.{level}]
  *
