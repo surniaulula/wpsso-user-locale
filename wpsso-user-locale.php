8e3b9c5308d16f6595740167cefc06a52d00ce79
JS Morisset <jsm@surniaulula.com>
2020-03-04 14:15:10 -0800

    v2.0.3

--- wpsso-user-locale.php
+++ wpsso-user-locale.php
@@ -11,7 +11,7 @@
  * License: GPLv3
  * License URI: http://www.gnu.org/licenses/gpl.txt
  * Description: Quick and easy locale / language / region selector for the WordPress admin toolbar.
- * Requires PHP: 5.5
+ * Requires PHP: 5.6
  * Requires At Least: 4.7
  * Tested Up To: 5.4
  * Version: 2.0.3
