a8eb76f93637f28d370629e8ddbbf27fe8288148
JS Morisset <jsm@surniaulula.com>
2020-04-06 10:33:15 -0700

    v2.1.0

--- wpsso-user-locale.php
+++ wpsso-user-locale.php
@@ -14,7 +14,7 @@
  * Requires PHP: 5.6
  * Requires At Least: 4.7
  * Tested Up To: 5.4
- * Version: 2.1.0-rc.3
+ * Version: 2.1.0
  *
  * Version Numbering: {major}.{minor}.{bugfix}[-{stage}.{level}]
  *
@@ -219,6 +219,11 @@ if ( ! class_exists( 'WpssoUl' ) ) {
 			}
 
 			if ( ! $this->have_min_version ) {
+
+				if ( $this->p->debug->enabled ) {
+					$this->p->debug->log( 'exiting early: have_min_version is false' );
+				}
+
 				return;	// Stop here.
 			}
 
