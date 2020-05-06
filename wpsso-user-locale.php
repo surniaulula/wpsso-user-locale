5dfdfa038eb9148d8b3ee7cedc31b92fec6feaa5
JS Morisset <jsm@surniaulula.com>
2020-05-06 11:18:39 -0700

    v2.2.0

--- wpsso-user-locale.php
+++ wpsso-user-locale.php
@@ -49,11 +49,14 @@ if ( ! class_exists( 'WpssoUl' ) ) {
 		/**
 		 * Reference Variables (config, options, modules, etc.).
 		 */
-		private $have_wpsso_min = true;	// Have WPSSO Core minimum version.
+		private $have_wpsso_min_version = true;	// Have WPSSO Core minimum version.
 
 		private static $wp_min_version = '4.7';	// Minimum WordPress version.
 
-		private static $instance;
+		private static $ext      = 'wpssoul';
+		private static $p_ext    = 'ul';
+		private static $info     = array();
+		private static $instance = null;
 
 		public function __construct() {
 
@@ -91,7 +94,7 @@ if ( ! class_exists( 'WpssoUl' ) ) {
 
 		public static function &get_instance() {
 
-			if ( ! isset( self::$instance ) ) {
+			if ( null === self::$instance ) {
 				self::$instance = new self;
 			}
 
@@ -103,32 +106,64 @@ if ( ! class_exists( 'WpssoUl' ) ) {
 		 */
 		public static function show_required_notices() {
 
-			$info = WpssoUlConfig::$cf[ 'plugin' ][ 'wpssoul' ];
+			$missing_requirements = self::get_missing_requirements();	// Returns false or an array of missing requirements.
 
-			foreach ( $info[ 'req' ] as $ext => $req_info ) {
+			if ( ! $missing_requirements ) {
+				return;	// Stop here.
+			}
 
-				if ( isset( $req_info[ 'class' ] ) ) {	// Just in case.
+			self::wpsso_init_textdomain();	// If not already loaded, load the textdomain now.
+
+			$info = WpssoUlConfig::$cf[ 'plugin' ][ self::$ext ];
+
+			$notice_msg = __( 'The %1$s add-on requires the %2$s plugin &mdash; please install and activate the missing plugin.',
+				'wpsso-user-locale' );
+
+			foreach ( $missing_requirements as $key => $req_info ) {
+
+				echo '<div class="notice notice-error error"><p>';
+
+				echo sprintf( $notice_msg, $info[ 'name' ], $req_info[ 'name' ] );
+
+				echo '</p></div>';
+			}
+		}
+
+		/**
+		 * Returns false or an array of the missing requirements (ie. 'wpsso', 'woocommerce', etc.).
+		 */
+		public static function get_missing_requirements() {
+
+			static $local_cache = null;
+
+			if ( null !== $local_cache ) {
+				return $local_cache;
+			}
+
+			$local_cache = array();
+
+			$info = WpssoUlConfig::$cf[ 'plugin' ][ self::$ext ];
+
+			foreach ( $info[ 'req' ] as $key => $req_info ) {
+
+				if ( isset( $req_info[ 'class' ] ) ) {
+
 					if ( class_exists( $req_info[ 'class' ] ) ) {
 						continue;	// Requirement satisfied.
 					}
-				} else continue;	// Nothing to check.
 
-				$deactivate_url = html_entity_decode( wp_nonce_url( add_query_arg( array(
-					'action'        => 'deactivate',
-					'plugin'        => $info[ 'base' ],
-					'plugin_status' => 'all',
-					'paged'         => 1,
-					's'             => '',
-				), admin_url( 'plugins.php' ) ), 'deactivate-plugin_' . $info[ 'base' ] ) );
+				} else {
+					continue;	// Nothing to check.
+				}
 
-				self::wpsso_init_textdomain();	// If not already loaded, load the textdomain now.
-
-				$notice_msg = __( 'The %1$s add-on requires the %2$s plugin &mdash; install and activate the plugin or <a href="%3$s">deactivate this add-on</a>.', 'wpsso-user-locale' );
-
-				echo '<div class="notice notice-error error"><p>';
-				echo sprintf( $notice_msg, $info[ 'name' ], $req_info[ 'name' ], $deactivate_url );
-				echo '</p></div>';
+				$local_cache[ $key ] = $req_info;
 			}
+
+			if ( empty( $local_cache ) ) {
+				$local_cache = false;
+			}
+
+			return $local_cache;
 		}
 
 		/**
@@ -182,13 +217,13 @@ if ( ! class_exists( 'WpssoUl' ) ) {
 		 */
 		public function wpsso_get_config( $cf, $plugin_version = 0 ) {
 
-			$info = WpssoUlConfig::$cf[ 'plugin' ][ 'wpssoul' ];
+			$info = WpssoUlConfig::$cf[ 'plugin' ][ self::$ext ];
 
 			$req_info = $info[ 'req' ][ 'wpsso' ];
 
 			if ( version_compare( $plugin_version, $req_info[ 'min_version' ], '<' ) ) {
 
-				$this->have_wpsso_min = false;
+				$this->have_wpsso_min_version = false;
 
 				return $cf;
 			}
@@ -201,14 +236,14 @@ if ( ! class_exists( 'WpssoUl' ) ) {
 		 */
 		public function wpsso_get_avail( $avail ) {
 
-			if ( ! $this->have_wpsso_min ) {
+			if ( ! $this->have_wpsso_min_version ) {
 
-				$avail[ 'p_ext' ][ 'ul' ] = false;	// Signal that this extension / add-on is not available.
+				$avail[ 'p_ext' ][ self::$p_ext ] = false;	// Signal that this extension / add-on is not available.
 
 				return $avail;
 			}
 
-			$avail[ 'p_ext' ][ 'ul' ] = true;		// Signal that this extension / add-on is available.
+			$avail[ 'p_ext' ][ self::$p_ext ] = true;		// Signal that this extension / add-on is available.
 
 			return $avail;
 		}
@@ -221,10 +256,10 @@ if ( ! class_exists( 'WpssoUl' ) ) {
 				$this->p->debug->mark();
 			}
 
-			if ( ! $this->have_wpsso_min ) {
+			if ( ! $this->have_wpsso_min_version ) {
 
 				if ( $this->p->debug->enabled ) {
-					$this->p->debug->log( 'exiting early: have_wpsso_min is false' );
+					$this->p->debug->log( 'exiting early: have_wpsso_min_version is false' );
 				}
 
 				return;	// Stop here.
@@ -243,7 +278,7 @@ if ( ! class_exists( 'WpssoUl' ) ) {
 				$this->p->debug->mark();
 			}
 
-			if ( ! $this->have_wpsso_min ) {
+			if ( ! $this->have_wpsso_min_version ) {
 
 				$this->min_version_notice();	// Show minimum version notice.
 
@@ -253,7 +288,7 @@ if ( ! class_exists( 'WpssoUl' ) ) {
 
 		private function min_version_notice() {
 
-			$info = WpssoUlConfig::$cf[ 'plugin' ][ 'wpssoul' ];
+			$info = WpssoUlConfig::$cf[ 'plugin' ][ self::$ext ];
 
 			$req_info = $info[ 'req' ][ 'wpsso' ];
 
