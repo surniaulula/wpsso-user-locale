fb6625c638e6053ef22f75ed515f6d38534d11ff
JS Morisset <jsm@surniaulula.com>
2020-08-11 12:57:09 -0700

    v2.5.0-rc.1

--- wpsso-user-locale.php
+++ wpsso-user-locale.php
@@ -14,7 +14,7 @@
  * Requires PHP: 5.6
  * Requires At Least: 4.7
  * Tested Up To: 5.5
- * Version: 2.5.0-b.3
+ * Version: 2.5.0-rc.1
  *
  * Version Numbering: {major}.{minor}.{bugfix}[-{stage}.{level}]
  *
