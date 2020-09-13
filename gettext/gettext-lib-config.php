df90547dc96176320f30c5b9b5fc7a617ad08e34
JS Morisset <jsm@surniaulula.com>
2020-09-13 12:14:03 -0700

    v2.5.0

--- gettext/gettext-lib-config.php
+++ gettext/gettext-lib-config.php
@@ -1,7 +1,7 @@
 <?php die( 'These aren\'t the droids you\'re looking for.' );
 
 /**
- * Extract of translatable strings from the config array.
+ * Extract of translatable strings from lib/config.php.
  */
 _x( 'User Locale', 'lib file description', 'wpsso-user-locale' );
 _x( 'Quick and Easy Locale / Language / Region Selector for the WordPress Admin Toolbar.', 'plugin description', 'wpsso-user-locale' );
