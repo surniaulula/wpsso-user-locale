a59634eaa4ab2733de880c835038393328d90e84
JS Morisset <jsm@surniaulula.com>
2020-09-15 09:49:33 -0700

    v2.5.1-b.2

--- gettext/gettext-lib-config.php
+++ gettext/gettext-lib-config.php
@@ -4,4 +4,5 @@
  * Extract of translatable strings from lib/config.php.
  */
 _x( 'User Locale', 'lib file description', 'wpsso-user-locale' );
+_x( 'WPSSO User Locale Selector', 'plugin name', 'wpsso-user-locale' );
 _x( 'Quick and Easy Locale / Language / Region Selector for the WordPress Admin Toolbar.', 'plugin description', 'wpsso-user-locale' );
