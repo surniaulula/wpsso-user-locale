3d5f1f4d5f4199914bdb67f8cb6e58f5d72ad397
JS Morisset <jsm@surniaulula.com>
2020-10-25 10:38:13 -0700

    v3.0.1

--- lib/config.php
+++ lib/config.php
@@ -91,7 +91,7 @@ if ( ! class_exists( 'WpssoUlConfig' ) ) {
 			return $add_slug ? $info[ 'slug' ] . '-' . $info[ 'version' ] : $info[ 'version' ];
 		}
 
-		public static function set_constants( $plugin_file ) { 
+		public static function set_constants( $plugin_file ) {
 
 			if ( defined( 'WPSSOUL_VERSION' ) ) {	// Define constants only once.
 
