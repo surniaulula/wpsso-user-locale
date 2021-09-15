04b1db2ef89e67ae25d169c0327c343631f1663f
JS Morisset <jsm@surniaulula.com>
2021-09-15 13:28:32 -0700

    v3.3.2

--- lib/filters.php
+++ lib/filters.php
@@ -49,11 +49,11 @@ if ( ! class_exists( 'WpssoUlFilters' ) ) {
 
 		public function filter_option_type( $type, $base_key ) {
 
-			if ( ! empty( $type ) ) {
+			if ( ! empty( $type ) ) {	// Return early if we already have a type.
 
 				return $type;
 
-			} elseif ( strpos( $base_key, 'ul_' ) !== 0 ) {
+			} elseif ( 0 !== strpos( $base_key, 'ul_' ) ) {	// Nothing to do.
 
 				return $type;
 			}
