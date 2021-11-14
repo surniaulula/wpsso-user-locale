c1d84de3d91d7466b235c3975f1b97e00b2ce2c7
JS Morisset <jsm@surniaulula.com>
2021-11-13 16:40:33 -0800

    v3.5.0

--- wpsso-user-locale.php
+++ wpsso-user-locale.php
@@ -67,10 +67,7 @@ if ( ! class_exists( 'WpssoUl' ) ) {
 			load_plugin_textdomain( 'wpsso-user-locale', false, 'wpsso-user-locale/languages/' );
 		}
 
-		/**
-		 * $is_admin, $doing_ajax, and $doing_cron available since WPSSO Core v8.8.0.
-		 */
-		public function init_objects( $is_admin = false, $doing_ajax = false, $doing_cron = false ) {
+		public function init_objects() {
 
 			$this->p =& Wpsso::get_instance();
 
