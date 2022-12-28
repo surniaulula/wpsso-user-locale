9657c7435a2f7da2b367c3be8ea316f2c8eab055
JS Morisset <jsm@surniaulula.com>
2022-12-28 12:10:27 -0800

    v3.7.0

--- gettext/gettext-lib-config.php
+++ gettext/gettext-lib-config.php
@@ -1,8 +1,7 @@
 <?php die( 'These aren\'t the droids you\'re looking for.' );
-
-/**
- * Extract of translatable strings from lib/config.php.
- */
+// 
+// Extract of translatable strings from lib/config.php.
+// 
 _x( 'User Locale', 'lib file description', 'wpsso-user-locale' );
 _x( 'WPSSO User Locale Selector', 'plugin name', 'wpsso-user-locale' );
 _x( 'Quick and easy locale / language / region selector for the WordPress admin toolbar.', 'plugin description', 'wpsso-user-locale' );
