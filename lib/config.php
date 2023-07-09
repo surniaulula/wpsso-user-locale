0e8e84e732445f0e55414fe76b7103697b58fcc6
JS Morisset <jsm@surniaulula.com>
2023-07-09 13:20:39 -0700

    v3.8.2-dev.5

--- lib/config.php
+++ lib/config.php
@@ -17,7 +17,7 @@ if ( ! class_exists( 'WpssoUlConfig' ) ) {
 		public static $cf = array(
 			'plugin' => array(
 				'wpssoul' => array(			// Plugin acronym.
-					'version'     => '3.8.2-dev.4',	// Plugin version.
+					'version'     => '3.8.2-dev.5',	// Plugin version.
 					'opt_version' => '7',		// Increment when changing default option values.
 					'short'       => 'WPSSO UL',	// Short plugin name.
 					'name'        => 'WPSSO User Locale Selector',
@@ -37,7 +37,7 @@ if ( ! class_exists( 'WpssoUlConfig' ) ) {
 							'home'          => 'https://wordpress.org/plugins/wpsso/',
 							'plugin_class'  => 'Wpsso',
 							'version_const' => 'WPSSO_VERSION',
-							'min_version'   => '15.15.2-dev.4',
+							'min_version'   => '15.15.2-dev.5',
 						),
 					),
 
